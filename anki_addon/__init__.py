b420344af4dc3b61b36f5bcc9edbe87361aa5fc0
P. v Petersenn <122872435+vonpetersenn@users.noreply.github.com>
2023-09-21 13:02:44 +0200

    initialize Addon
    initialise Addon structure

---
+++ anki_addon/__init__.py
@@ -0,0 +1,23 @@
+# import the main window object (mw) from aqt
+from aqt import mw
+# import the "show info" tool from utils.py
+from aqt.utils import showInfo, qconnect
+# import all of the Qt GUI library
+from aqt.qt import *
+
+# We're going to add a menu item below. First we want to create a function to
+# be called when the menu item is activated.
+
+def testFunction() -> None:
+    # get the number of cards in the current collection, which is stored in
+    # the main window
+    cardCount = mw.col.cardCount()
+    # show a message box
+    showInfo("Card count: %d" % cardCount)
+
+# create a new menu item, "test"
+action = QAction("import Bookmarks from Textfile", mw)
+# set it to call testFunction when it's clicked
+qconnect(action.triggered, testFunction)
+# and add it to the tools menu
+mw.form.menuTools.addAction(action)
