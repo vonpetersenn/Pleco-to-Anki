4b241e187bff233bf85304e0ff93fc96462fd1ca
P. v Petersenn <122872435+vonpetersenn@users.noreply.github.com>
2023-09-22 15:58:43 +0200

    GUI now functional

--- anki_addon/GUI.py
+++
@@ -1,4 +0,0 @@
-from aqt import mw
-from aqt.qt import *
-from aqt.utils import showInfo
-
---
+++ anki_addon/create_anki_cards.py
@@ -0,0 +1,13 @@
+def create_anki_cards(configs = "configs"):
+
+    nice_string = ""
+
+    nice_string += "Reformat pinyin: " + str(configs.reformat_pinyin) + "\n"
+    nice_string += "Reformat keywords: " + str(configs.reformat_keywords) + "\n"
+    nice_string += "Reformat example sentences: " + str(configs.reformat_example_sentences) + "\n"
+    nice_string += "Group example sentences: " + str(configs.group_example_sentences) + "\n"
+    nice_string += "File name: " + str(configs.file_name) + "\n"
+    nice_string += "Spoonfed examples: " + str(configs.spoonfed_examples) + "\n"
+    nice_string += "Traditional or simplified: " + str(configs.trad_or_simp) + "\n"
+
+    return nice_string
